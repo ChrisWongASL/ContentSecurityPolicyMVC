f07dd76cb8f5fa311f636d0963f5773eec5aa0d9
chris.wc.wong <chris.wc.wong.ps.asl@gmail.com>
2022-09-14 11:48:57 +0800

    Add project files.

---
+++ ContentSecurityPolicyMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContentSecurityPolicyMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
